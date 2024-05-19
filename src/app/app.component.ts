242a343a85cb0db1e66d0afc55bc65b04b899e72
DanielR <danielr.dv9@gmail.com>
2024-05-19 15:04:06 -0500

    Finalizada primera version de la maquetacion en todas las vistas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { HeaderNavComponent } from './layout/components/header-nav/header-nav.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeaderNavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sachi-pet';
+  title = 'Sachi Pet - Tienda Online';
 }
